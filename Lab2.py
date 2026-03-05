a236343908b5a0e1b9c6a71c2c4bb1c1f2be80d8
ericericeric-3k <camotajohnerick@gmail.com>
2026-03-05 22:12:16 +0800

    Changed process

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as FUNC
 
 spark = SparkSession.builder \
     .appName("ProcessStudentData") \
@@ -9,6 +10,9 @@ sc = spark.sparkContext
 
 sc.setLogLevel("ERROR")
 
-dataset = spark.read.csv("dataset/ultimate_student_productivity_dataset_5000.csv", header=True, inferSchema=True)
+dataset = spark.read.option("header", "true").csv("dataset/ultimate_student_productivity_dataset_5000.csv")
+
+data = dataset.rdd
+
 
 
