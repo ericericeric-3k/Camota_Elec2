131e716221bd71711d33de80177eaeae7e111e99
ericericeric-3k <camotajohnerick@gmail.com>
2026-03-11 20:49:41 +0800

    New Test

---
+++ lab3.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder \
+    .appName("Topmost Delayed Departure by Trip Count") \
+    .master("local[*]") \
+    .getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+
+# Preparing the Data: Loading, selecting
+trips_df = spark.read.csv("dataset/departuredelays.csv", header=True, inferSchema=True)
+trips_select = trips_df.select("origin", "destination", "delay", "distance")
+
+# Processing the Data: Grouping, aggregating, and ordering(by the aggregate trips and the average delay for each)
+trips_group = trips_select.groupBy("origin", "destination").agg(F.round(F.avg("delay"), 2).alias("avg delay"), F.count("*").alias("total trips"))
+trips_order = trips_group.orderBy(F.col("total trips").desc(), F.col("avg delay").desc())
+
+# Displaying the Data: Limiting the results
+top_delayed = trips_order.limit(20)
+top_delayed.show(top_delayed.count(), truncate=False)
+
+spark.stop()
